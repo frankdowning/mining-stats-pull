d2b623dd2b175d3bead0599f3dfbc41aa24df934
frankdowning <fdowning@umich.edu>
2020-12-11 23:21:04 -0600

    Add remote logging?

--- pull.py
+++ pull.py
@@ -12,36 +12,50 @@ def get_dashboard_data(address):
     try:
         r = requests.get(endpoint + path)
     except Exception as e:
-        print(f"Failed to get data for {address} - {e}")
+        msg = f"Failed to get data for {address} - {e}"
+        print(msg)
+        logs.insert_one({'msg': msg})
 
     return r.json()
 
 def store_dashboard_data(collection, address, data):
     try:
         id = collection.insert_one(data).inserted_id
-        print(f"Wrote data for {id}")
+        msg = f"Wrote data for {id}"
+        print(msg)
+        logs.insert_one({'msg': msg})
+
     except Exception as e:
-        print(f"Failed to store data for {address} - {e}")
+        msg = f"Failed to store data for {address} - {e}"
+        print(msg)
+        logs.insert_one({'msg': msg})
 
 print("Start")
 
 MONGO_URL = os.environ['MONGO_URL']
 DB_TIER = os.environ['DB_TIER']
 client = MongoClient(MONGO_URL)
-print(f"Connected to {MONGO_URL} in {DB_TIER}")
 db = client[DB_TIER]
 
 addresses = db.addresses
 mining_stats = db.mining_stats
+logs = db.logs
+
+msg = f"Connected to {MONGO_URL} in {DB_TIER}"
+print(msg)
+logs.insert_one({'msg': msg})
 
 for address in addresses.find():
     data = get_dashboard_data(address['address'])
 
     if data['status'] == 'OK':
         store_dashboard_data(mining_stats, address, data)
-        print(f"Successfuly stored data for {address['address']}!")
+        msg = f"Successfuly stored data for {address['address']}!"
+        print(msg)
+        logs.insert_one({'msg': msg})
     else:
-        print(f"Failed to get data for {address} - {data['status']}")
-
+        msg = f"Failed to get data for {address} - {data['status']}"
+        print(msg)
+        logs.insert_one({'msg': msg})
 
 print("Complete")
